cc7196ba2bf04a67fc6ae6411e12d761733235b9
胡赞來 <361206768@qq.com>
2023-02-19 23:12:44 +0800

    chore: 调整singleproject组件

--- my-app/src/components/SingleProject/index.tsx
+++ my-app/src/components/SingleProject/index.tsx
@@ -9,11 +9,22 @@ export interface SingleProjectProps {
 const SingleProject = (props: SingleProjectProps) => {
   return (
     <div className='single-project-content'>
-      <div className='single-project-title'>{props.title}</div>
-      {props.desc.map(item => (
-        <div className='single-project-desc'>{item}</div>
-      ))}
-      <div className='single-project-view'>view more {props.link}</div>
+      <div>
+        <div className='single-project-title'>{props.title}</div>
+        {props.desc.map(item => (
+          <div className='single-project-desc'>{item}</div>
+        ))}
+      </div>
+      <div className='single-project-view'>
+        <a
+          target='_blank'
+          rel='noreferrer'
+          className='weblink'
+          href={props.link}
+        >
+          view more
+        </a>
+      </div>
     </div>
   )
 }
