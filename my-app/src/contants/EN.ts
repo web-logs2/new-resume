b0928ea9dd5b198a831aea5ee6ada12a7c214bb8
胡赞來 <361206768@qq.com>
2023-02-19 23:05:03 +0800

    chore: 调整文案

--- my-app/src/contants/EN.ts
+++ my-app/src/contants/EN.ts
@@ -1,14 +1,14 @@
 export const PersonalWebsiteUrl = "https://zanlaihu.github.io/blog/"
 
-export const EducationConstant = {
-    title: "教育经历"
-}
-
 export const ProfileConstant = {
-    title: "胡赞来 / 简历",
+    title: "Zanlai Hu / Resume",
     phone: '13867908869',
     email: 'klaushu@outlook.com',
     web: "个人网站："
+}
+
+export const EducationConstant = {
+    title: "教育经历"
 }
 
 export const ProjectConstant = {
@@ -18,7 +18,7 @@ export const ProjectConstant = {
         { title: "大转盘", desc: ['钱包APP的大转盘抽奖功能。'] },
         { title: "折扣券", desc: ['在钱包APP已有的支付功能中加入可选折扣券，调整支付流程。'] },
         { title: "KYC", desc: ['钱包APP中用户录入个人信息的功能'] },
-        { title: "线下扫码支付", desc: ['钱包APP的线下门店扫码支付功能'] },
+        { title: "OTC", desc: ['钱包APP的线下门店扫码支付功能'] },
         { title: "钱包APP组件库", desc: ['专用于钱包APP的组件库，开发了list、input、dialog、toast、drawer、progress等组件'] },
         { title: "批量代发", desc: ['批量代发全称为批量代发工资，是BO中后台提供的功能。入驻商家可以通过该功能在BO平台上给员工分发工资。'] },
     ]
@@ -33,7 +33,7 @@ export const WorkConstant = {
     title: "工作经历",
     works: [{
         title: "蚂蚁集团",
-        year:"2021.10 - 至今",
+        year: "2021.10 - 至今",
         desc: [""]
     }, {
         title: "",
